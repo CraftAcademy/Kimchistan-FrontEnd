d154f2340a90189477fd0daba87e9b604d3e8625
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-11-14 11:44:05 +0100

    trying to display amount of dish

--- src/app/service/shoppingcart.service.ts
+++ src/app/service/shoppingcart.service.ts
@@ -5,16 +5,29 @@ import {Injectable} from '@angular/core';
 export class ShoppingCartService {
   cart: any = [];
 
-  addToCart(p_id: string, p_name: string, price: number, i_id: string, i_name: string) {
-
-    this.cart.push({
-      'product_id': p_id,
-      'product_name': p_name,
-      'price': Number(price),
+  addToCart(p_id: number, p_name: string, price: number, i_id: string, i_name: string) {
+  if (this.cart.length != 0) {
+    this.cart.forEach(function(obj) {
+        if (p_id === obj.product_id) {
+          obj.item_quantity = (obj.item_quantity || 0) + 1;
+          console.log(obj.item_quantity);
+        } else {
+          console.log("hello");
+          this.addItemToCart(p_id, p_name, price, i_id, i_name);
+          console.log(this.cart);
+        }
+      }
+    } else {
+      this.cart.push({
+                       'product_id': p_id,
+                       'product_name': p_name,
+                       'price': Number(price),
       'ingredient_id': i_id,
-      'ingredient_name': i_name
+      'ingredient_name': i_name,
+      'item_quantity': 1
     });
     console.log(this.cart);
+    }
   }
 
   showAll() {
@@ -26,7 +39,17 @@ export class ShoppingCartService {
     this.cart.forEach(function(obj){
       total_price += obj.price;
     });
-    console.log(total_price);
     return total_price;
+  }
+
+  addItemToCart(p_id: number, p_name: string, price: number, i_id: string, i_name: string) {
+    this.cart.push({
+      'product_id': p_id,
+      'product_name': p_name,
+      'price': Number(price),
+      'ingredient_id': i_id,
+      'ingredient_name': i_name,
+      'item_quantity': 1
+    });
   }
 }
