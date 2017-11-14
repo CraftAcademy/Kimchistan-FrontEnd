fa1e10580e4b88afddeebe501d81edc15efd909c
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-11-14 12:16:31 +0100

    getting quantity, but collapsable is failing

--- src/app/service/shoppingcart.service.ts
+++ src/app/service/shoppingcart.service.ts
@@ -5,28 +5,24 @@ import {Injectable} from '@angular/core';
 export class ShoppingCartService {
   cart: any = [];
 
-  addToCart(p_id: number, p_name: string, price: number, i_id: string, i_name: string) {
-  if (this.cart.length != 0) {
-    this.cart.forEach(function(obj) {
-        if (p_id === obj.product_id) {
-          obj.item_quantity = (obj.item_quantity || 0) + 1;
-          console.log(obj.item_quantity);
-        } else {
-          console.log("hello");
-          this.addItemToCart(p_id, p_name, price, i_id, i_name);
-          console.log(this.cart);
-        }
+  addToCart(p_id: string, p_name: string, price: number, i_id: string, i_name: string) {
+    let itemFound = false;
+    this.cart.forEach(function (obj) {
+      if (p_id === obj.product_id) {
+        obj.item_quantity += 1;
+        itemFound = true;
       }
-    } else {
-      this.cart.push({
-                       'product_id': p_id,
-                       'product_name': p_name,
-                       'price': Number(price),
-      'ingredient_id': i_id,
-      'ingredient_name': i_name,
-      'item_quantity': 1
     });
-    console.log(this.cart);
+    if (!itemFound) {
+      this.cart.push({
+        'product_id': p_id,
+        'product_name': p_name,
+        'price': Number(price),
+        'ingredient_id': i_id,
+        'ingredient_name': i_name,
+        'item_quantity': 1
+      });
+      console.log(this.cart);
     }
   }
 
@@ -36,20 +32,9 @@ export class ShoppingCartService {
 
   showTotal() {
     let total_price = 0;
-    this.cart.forEach(function(obj){
+    this.cart.forEach(function (obj) {
       total_price += obj.price;
     });
     return total_price;
-  }
-
-  addItemToCart(p_id: number, p_name: string, price: number, i_id: string, i_name: string) {
-    this.cart.push({
-      'product_id': p_id,
-      'product_name': p_name,
-      'price': Number(price),
-      'ingredient_id': i_id,
-      'ingredient_name': i_name,
-      'item_quantity': 1
-    });
   }
 }
