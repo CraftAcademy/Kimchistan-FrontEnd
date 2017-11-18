8f8855274bb571ad8502d863d4e9a2f114780f93
magnus-thor <magnus.thor.ar@gmail.com>
2017-11-18 17:59:29 +0100

    removed unused method

--- src/app/service/payment.service.ts
+++ src/app/service/payment.service.ts
@@ -10,15 +10,15 @@ export class PaymentService {
   constructor(private datastore: DatastoreService, private http: Http) {
   }
 
-  create(token: any, amount: any) {
-    const payment = this.datastore.createRecord(Payment, {
-      email: token.email,
-      token: token.id,
-      amount: amount,
-    });
-
-    return payment.save();
-  }
+  // create(token: any, amount: any) {
+  //   const payment = this.datastore.createRecord(Payment, {
+  //     email: token.email,
+  //     token: token.id,
+  //     amount: amount,
+  //   });
+  //
+  //   return payment.save();
+  // }
 
   createPayment(token: any, amount: any) {
     const apiUrl = 'https://kimchistan-api.herokuapp.com/api/v1'
@@ -30,7 +30,7 @@ export class PaymentService {
           amount: amount
         }
       }
-    }
+    };
 
     return this.http.post(`${apiUrl}/payments`, payload)
       .map(res => res.json());
